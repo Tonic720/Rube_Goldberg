0044a91bc956c11d04f0600189ea0868917ed15e
Unknown <tonicwater720@gmail.com>
2017-07-18 06:19:30 -0400

    Level completed all code and models made along with music

---
+++ Assets/Scritps/Finish_Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finish_Trigger : MonoBehaviour {
+	public Text finishText;
+
+	int counter = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (counter > 2) {
+
+			finishText.text = "FINISH!!!";
+		}
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		counter++;
+	}
+}
